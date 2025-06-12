484f1792bf86458db42f8656ddda7b93923619db
harunsanduvac38 <harunsanduvac@gmail.com>
2025-06-12 13:21:56 +0200

    first commit

---
+++ src/com/objetos/Prueba.java
@@ -0,0 +1,11 @@
+package com.objetos;
+
+public class Prueba {
+    // Esta clase no se va a ejecutar
+    // Creamos un metodo para saludar
+
+    public static void saludar(){
+        System.out.println("Hola a todo desde Prueba!");
+    }
+    
+}
