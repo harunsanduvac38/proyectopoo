46522bf6ba8d2b3682e30a7c3dd3e63b3b0e2efd
harunsanduvac38 <harunsanduvac@gmail.com>
2025-06-12 13:53:23 +0200

    poo

--- src/App.java
+++ src/App.java
@@ -1,9 +1,19 @@
-import com.objetos.Prueba;
+import com.objetos.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Probando la clase Persona");
 
-        Prueba.saludar();
+        // Cuando hablamos de clases, necesitamos objetos para trabajar con dichas clases
+        // Instanciamos el objeto persona
+
+        Persona person = new Persona();
+        // Ya podemos utilizar la persona y sus propiedades :
+        person.nombre = "Lucas";
+        person.apellidos = "Lopez";
+        person.edad = 29;
+        System.out.println(person.nombre + " " + person.apellidos + " " + "Edad: " + person.edad);
+
+        System.out.println("Fin de Programa");
     }
 }
