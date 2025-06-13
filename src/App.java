cfe5bb907f1aea7b885bb8b5573ea8cb173da1b4
harunsanduvac38 <harunsanduvac@gmail.com>
2025-06-13 10:07:24 +0200

    Persona

--- src/App.java
+++ src/App.java
@@ -2,18 +2,26 @@ import com.objetos.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Probando la clase Persona");
+        System.out.println("\n Probando la clase Persona");
 
         // Cuando hablamos de clases, necesitamos objetos para trabajar con dichas clases
         // Instanciamos el objeto persona
 
         Persona person = new Persona();
         // Ya podemos utilizar la persona y sus propiedades :
-        person.nombre = "Lucas";
-        person.apellidos = "Lopez";
-        person.edad = 29;
-        System.out.println(person.nombre + " " + person.apellidos + " " + "Edad: " + person.edad);
+        person.setNombre("Luis");
+        // person.apellidos = "Lopez";
+        // person.edad = 29;
+        System.out.println(person.getNombre());
 
-        System.out.println("Fin de Programa");
+
+        person.setApellidos("Lopez");
+        System.out.println(person.getApellidos());
+        
+
+        person.setEdad(23);
+        System.out.println(person.getEdad());
+
+        System.out.println("Fin de Programa \n");
     }
 }
--- src/com/objetos/Persona.java
+++ src/com/objetos/Persona.java
@@ -1,8 +1,41 @@
 package com.objetos;
 
 public class Persona {
-    public String nombre;
-    public String apellidos;
-    public int edad;
+    // debemos declarar las variables privadas de cada propiedad
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        // debemos controlar la edad y realizar la acción que más nos guste
+        // 1)error silencioso: El programador no sabe que ha hecho algo mal, simplemente lo soluciono yo aquí
+        if(edad < 0){
+            this.edad = 0;
+        }else {
+            this.edad = edad;
+        }
+        
+    }
+
+    // Por cada propiedad tenemos dos metodos  (Get y Set) y get para recuperar, set para almacenar
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellidos(){
+        return this.apellidos;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
     
 }
